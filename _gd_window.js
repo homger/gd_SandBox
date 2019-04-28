63b6f790ea3b39a27a035fbd0d6429686955af27
Guerchôm <d.homger@gmail.com>
2019-04-28 22:24:12 +0200

    Save

--- _gd_window.js
+++ _gd_window.js
@@ -1,34 +1,91 @@
 'use strict';
 
+function get_offsetXY(element){
+    let e = element.parentNode;
+    let top = 0, left = 0;
 
-function a(){
-
+    while(e.nodeName != document.nodeName){
+        console.log(e.nodeName);
+        top += e.offsetTop;
+        left += e.offsetLeft;
+        e = e.parentNode;
+    }
+    return{
+        top,
+        left,
+    }
 }
-function _gd_window(htmlBlockType = "div"){
-    let block = document.createElement(htmlBlockType);
 
+
+function _gd_window(htmlBlockElementToMove,boundingBlock = document.body, default_z_index = -1, moving_z_index = 2){
+    let block = document.createElement("div");
+    block.style.position = "absolute";
+    //block.style.boxSizing = "border-box";
+    block.style.top = "0";
+    block.style.left = "0";
+    block.style.width = "100%";
+    block.style.height = "100%";
+    block.default_z_index = default_z_index;
+    block.moving_z_index = moving_z_index;
+
+    block._top = 0, block._left = 0;
+    let offsetObj = get_offsetXY(boundingBlock);
+    block._offsetTop = offsetObj.top;
+    block._offsetLeft = offsetObj.left;
     block._move = function(e){
+        let W = block.offsetWidth;
+        let H = block.offsetHeight;
         if(block._m_down){
-            console.log(e.x);
-            block.style.top = e.y - block._of_y + "px";
-            block.style.left = e.x - block._of_x + "px";
+            block._top = e.y - block._of_y;
+            block._left = e.x - block._of_x;
+            if(block._top > boundingBlock.clientHeight + block._offsetTop - H)
+                block._top = boundingBlock.clientHeight + block._offsetTop  - H;
+            else if(block._top < block._offsetTop)
+                block._top = block._offsetTop;
+            
+            if(block._left > boundingBlock.clientWidth + block._offsetLeft - W)
+                block._left = boundingBlock.clientWidth + block._offsetLeft - W;
+            else if(block._left < block._offsetLeft)
+                block._left = block._offsetLeft;
+            
+
+            htmlBlockElementToMove.style.top = block._top  - block._offsetTop + "px";
+            htmlBlockElementToMove.style.left = block._left - block._offsetLeft + "px";
         }
     };
-
+    block._stop = function(e){
+        if(block._m_down){
+            block._m_down = false;
+            block.style.zIndex = block.default_z_index;
+            window.removeEventListener("mouseup", block._stop);
+            //boundingBlock.removeEventListener("mouseleave", block._stop);
+            window.removeEventListener("mousemove", block._move);
+        }
+    }
     block.addEventListener("mousedown", function(event){
-        block._of_x = event.offsetX;
-        block._of_y = event.offsetY;
-        block._m_down = true;
-        window.addEventListener("mousemove", block._move);
+        if(!block._m_down){
+            block._of_x = event.offsetX;
+            block._of_y = event.offsetY;
+            block.style.zIndex = block.moving_z_index;
+            window.addEventListener("mousemove", block._move);
+            //boundingBlock.addEventListener("mouseleave", block._stop);
+            window.addEventListener("mouseup", block._stop);
+            block._m_down = true;
+            console.log("DOWN");
+        }
     });
-    /*block.addEventListener("mouseleave", function(){
-        block._m_down = false;
-    });*/
+    
     block.addEventListener("mouseup", function(){
-        block._m_down = false;
-        window.removeEventListener("mousemove", block._move);
+        if(block._m_down){
+            block._m_down = false;
+            block.style.zIndex = block.default_z_index;
+            window.removeEventListener("mousemove", block._move);
+            console.log("UP");
+        }
     });
-    //block.addEventListener("mousemove", );
+
+    htmlBlockElementToMove.appendChild(block);
+    
     
     return block;
 }
--- gd_SandBox.js
+++ gd_SandBox.js
@@ -12,14 +12,56 @@ class gd_SandBox{
         </section>
         <footer></footer>
         `;
-
-        let d = _gd_window("section");
+        
+        let d = document.createElement("section");
         d.className = "view-section";
         let iframe = document.createElement("iframe");
-        iframe.src = "https://youtu.be/NZHzgXFKfuY";
-        //d.appendChild(iframe);
+        iframe.src = "w";
+        d.appendChild(iframe);
         this.container.className ="cc";
         this.container.appendChild(d);
         container.appendChild(this.container);
+        _gd_window(d, this.container);
+
+        //test(d);
     }
+}
+function test(_div){
+    let d = document.querySelector(".view-section");
+    let c = d;
+    console.log(c.offsetLeft + " " + c.offsetTop +" __node name:" + c.nodeName);
+    let i = 0;
+    let left = 0;
+    let top = 0;
+    let a = c;
+    do{
+        /*if(i==1){
+            let a = c;
+            setTimeout(() => {
+                console.log("GONA DO");
+                console.log(a.offsetTop);
+                a.style.left = 0 + "px";
+                a.style.top = 0 + "px";
+
+            }, 1000);
+        }*/
+        if(c.offsetLeft !== undefined){
+            left += c.offsetLeft;
+            top += c.offsetTop;
+        }
+        c = c.parentNode;
+        console.log(c.offsetLeft + " " + c.offsetTop +" __node name:" + c.nodeName);
+        ++i;
+    }while(c.offsetLeft !== undefined);
+
+    setTimeout(() => {
+        console.log("GONA DO");
+        console.log(a.offsetTop);
+
+        a.style.position = "fixed";
+        a.style.right = left - a.clientWidth  + "px";
+        a.style.bottom = top - a.clientHeight + "px";
+
+    }, 1000);
+    
 }
